558e66f1e36e9b77ddf08a59a19a816431a88d85
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-03 11:38:53 +0800

    push first comment in my front end project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  welcome to aiu
+</template>
+
+<style scoped>
+:root {
+  --primary-color: #c38b1a;
+  --secondary-color: #c68414;
+  --background-color: #F5F5F5;
+  --text-color: #121214;
+}
+
+</style>
