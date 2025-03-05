2acb7ef7f4bb6fcd08965785d0ac093b97d28f24
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-05 12:45:51 +0800

    add new structures to app include colors and font sizes

--- app.vue
+++ app.vue
@@ -3,15 +3,29 @@
 </template>
 
 
-<style >
+<style>
 :root {
-  --primary-color: #c38b1a;
-  --secondary-color: #c68414;
-  --background-color: #eeeeee;
+
+  --primary-color: #d4a525;
+  --secondary-color: #c38b1a;
+  --background-color: #111111;
+  --text-color: #d4a525;
+  --text-hover: #f5d67a;
+  --border-color: #333333;
   --text-prayer-color: #1e265a;
-  --text-color: #eeeeee;
-  --text-hover: #f5f5f5;
+
   --spacing-unit: 1rem;
+  --primary-border-radius: 15px;
+
+  --text-size-h1: clamp(3rem, 5vw, 4rem);
+  --text-size-h2: clamp(2.5rem, 4vw, 3.5rem);
+  --text-size-h3: clamp(2rem, 3.5vw, 3rem);
+  --text-size-h4: clamp(1.75rem, 3vw, 2.5rem);
+  --text-size-h5: clamp(1.5rem, 2.5vw, 2rem);
+  --text-size-h6: clamp(1rem, 2vw, 1.5rem);
+
+  --text-size-body: 1rem;
+  --text-size-small: 0.875rem;
 }
 
 </style>
