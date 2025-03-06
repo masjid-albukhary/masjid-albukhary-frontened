5e324dba608813f2d449826bb5b6f9e50b9b6223
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-06 11:59:25 +0800

    created contact page and contact component

---
+++ components/Contact.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="contact-section">
+    <div class="container">
+      <div class="contact-info">
+
+      </div>
+      <div class="contact-info-form">
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+section {
+  width: 100%;
+  height: 100%;
+  background: var(--bg-color);
+  margin: 3rem auto;
+}
+
+
+.contact-section div{
+  max-width: 1200px;
+  margin: 2rem auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import Contact from "~/components/Contact.vue";
+</script>
+
+<template>
+  <NavBar/>
+  <Contact/>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
