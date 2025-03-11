dea24b082f82b1f0226e9ca1e4ff4ff27ba913af
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-11 15:45:58 +0800

    remove unneeded components in index page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 
 import AdminNavBar from "~/components/AdminNavBar.vue";
+import ImageUploadPopup from "~/components/ImageUploadPopup.vue";
 </script>
 
 <template>
   <AdminNavBar/>
+  <ImageUploadPopup/>
   <AdminFooter/>
 </template>
 
