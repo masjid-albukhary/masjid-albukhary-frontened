2323501fd55004e06d62f78794d64dd4ba4191db
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-13 08:47:04 +0800

    Add GitHub Actions deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  colorMode: {
+    preference: 'light'
+  },
+  app: {
+    baseURL: process.env.NODE_ENV === 'development' ? '' : '/masjid-albukhary-frontend/',
+  },
+  nitro: {
+    preset: 'github_pages'
+  },
+  runtimeConfig: {
+    public: {
+      isDev: process.env.NODE_ENV === 'development',
+    },
+  },
 })
