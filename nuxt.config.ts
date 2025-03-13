49c2d4863c7f2279cf35b18557f87bcfc42a2836
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-13 09:41:29 +0800

    fixing issues that i am facing in deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  colorMode: {
-    preference: 'light'
-  },
-  app: {
-    baseURL: process.env.NODE_ENV === 'development' ? '' :  '/masjid-albukhary-frontend/',
-  },
-  nitro: {
-    preset: 'github_pages'
-  },
-  runtimeConfig: {
-    public: {
-      isDev: process.env.NODE_ENV === 'development',
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: ['@nuxt/ui'],
+    colorMode: {
+        preference: 'light'
     },
-  },
+    pages: true,
+    app: {
+        baseURL: process.env.NODE_ENV === 'development' ? '' : '/masjid-albukhary-frontend/',
+    },
+    nitro: {
+        preset: 'github_pages'
+    },
+    runtimeConfig: {
+        public: {
+            isDev: process.env.NODE_ENV === 'development',
+        },
+    },
 })
