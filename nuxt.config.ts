f935370f9b5fb66f3ffeecc606beedb9b5b77521
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-17 10:50:02 +0800

    removed nuxtjs i18n from

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui',],
   app: {
     baseURL: process.env.NODE_ENV === 'development' ? '' : '/masjid-albukhary-frontened/',
   },
