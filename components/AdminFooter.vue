058cccae6b8a6e4f33031fcf5b85582ccdb74897
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-24 09:16:58 +0800

    fixed link issues in footer header

--- components/AdminFooter.vue
+++ components/AdminFooter.vue
@@ -4,24 +4,38 @@ const currentYear = new Date().getFullYear();
 </script>
 
 <template>
-  <div class="footer-admin-section">
+  <footer class="footer-admin-section">
     <div class="footer-container">
       <p>&copy; Copyright <a href="https://aiu.edu.my/">Al Bukhary Mosque {{ currentYear }}</a></p>
     </div>
-  </div>
+  </footer>
 </template>
 
 
 <style scoped>
-.footer-admin-section {
+footer {
   margin: auto;
   background-color: var(--background-color);
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    color: var(--text-color);
+    transition: color 0.3s ease;
+    text-align: center;
+  }
+
+  a:hover {
+    text-decoration: underline;
+    color: var(--text-hover);
+    transition: color 0.3s ease;
+  }
 }
 
 .footer-container {
   display: flex;
   justify-content: space-between;
-  margin: 0  auto;
+  margin: 0 auto;
   text-align: center;
   font-size: 1.2rem;
   padding: .5rem;
@@ -30,7 +44,4 @@ const currentYear = new Date().getFullYear();
   background-color: var(--primary-color);
 }
 
-a:hover {
-  color: var(--text-hover);
-}
 </style>
