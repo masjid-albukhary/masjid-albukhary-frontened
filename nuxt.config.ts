99361e253de640061ba1771427db7652189b8c72
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-25 16:16:40 +0800

    trying to fix deployment after connected domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxtjs/i18n'],
   app: {
-    baseURL: process.env.NODE_ENV === 'development' ? '' : '/masjid-albukhary-frontened/',
+    baseURL: '',
   },
   i18n: {
     locales: [
