accb88273da107a9c95c1c410232d6a302e725d0
adnanmadi417 <adnanmadi417@gmail.com>
2025-04-06 03:23:25 +0800

    creating users admin dashboard , services  admin dashboard page  and updated sidebar and related files

---
+++ pages/admin/users-dashboard.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+import AdminHeader from "~/components/AdminHeader.vue";
+import AdminSideBar from "~/components/AdminSideBar.vue";
+
+definePageMeta({
+  middleware: 'auth'
+})
+
+</script>
+
+<template>
+  <AdminHeader/>
+  <section class="users-container">
+    <aside class="users-aside">
+      <AdminSideBar/>
+    </aside>
+    <main>
+    </main>
+  </section>
+  <AdminFooter/>
+</template>
+
+<style scoped>
+section {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  gap: 20px;
+}
+
+.users-aside {
+  display: flex;
+  background-color: var(--bg-color);
+}
+
+main {
+  padding: 20px;
+}
+
+@media (max-width: 1024px) {
+  section {
+    grid-template-columns: 1fr;
+  }
+
+  .users-aside {
+    justify-content: center;
+  }
+}
+
+@media (max-width: 768px) {
+  section {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
