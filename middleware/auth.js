47fa5083495d7a94113926c83a8920f676e6007e
adnanmadi417 <adnanmadi417@gmail.com>
2025-04-15 02:37:37 +0800

    updated auth logic

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,13 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const token = useCookie('token');
     const refresh = useCookie('refresh_token');
+    const isAuthenticated = token.value && refresh.value;
 
-    const bothThere = token.value && refresh.value;
-
-    if (!bothThere && to.path !== '/login') {
+    if (!isAuthenticated && to.path !== '/user-login') {
         return navigateTo('/user-login');
     }
 
-    if (token.value && to.path === '/user-login') {
+    if (isAuthenticated && to.path === '/user-login') {
         return navigateTo('/admin');
     }
 });
