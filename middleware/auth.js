eb3253a0643d2b38cc4f79da2b085849d95e06d3
adnanmadi417 <adnanmadi417@gmail.com>
2025-04-24 23:51:43 +0800

    commenting for debugging purposes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,14 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    const token = useCookie('token', { sameSite: 'lax' });
-    const refresh = useCookie('refresh_token', { sameSite: 'lax' });
+    const token = useCookie('token', {sameSite: 'lax'});
+    const refresh = useCookie('refresh_token', {sameSite: 'lax'});
 
     const isAuthenticated = !!token.value && !!refresh.value;
 
-    if (!isAuthenticated && to.path !== '/user-login') {
-        return navigateTo('/user-login');
-    }
+    console.log(isAuthenticated);
+
+    // if (!isAuthenticated && to.path !== '/user-login') {
+    //     return navigateTo('/user-login');
+    // }
 
     if (isAuthenticated && to.path === '/user-login') {
         return navigateTo('/admin');
