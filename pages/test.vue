ca4c34b4616cf77232d1048ed524b3e391ff8c3d
adnanmadi417 <adnanmadi417@gmail.com>
2025-04-28 14:24:45 +0800

    added test landing to test page

--- pages/test.vue
+++ pages/test.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 
 import PrayerTimes from "~/components/PrayerTimes.vue";
+import TestLanding from "~/components/TestLanding.vue";
 </script>
 
 <template>
   <PrayerTimes/>
+  <TestLanding/>
+  <Gallery/>
 </template>
 
 <style scoped>
